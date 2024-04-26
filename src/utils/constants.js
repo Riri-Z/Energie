d42073e40cea0b9527172504a14c1414ed539aa7
Riri-Z <riri.zamor@gmail.com>
2024-04-26 12:03:03 +0200

    Remove unused variables

--- src/utils/constants.js
+++ src/utils/constants.js
@@ -1,23 +1,2 @@
-// A REVOIR LES URLS  EN FONCTION DES CHARTS SOUHAITES
-export const BASE_ODRE_URL = 'https://odre.opendatasoft.com';
-export const FILTERED_API_URL =
-  'https://odre.opendatasoft.com/api/explore/v2.1/catalog/datasets/eco2mix-national-tr/records?limit=100&refine=date_heure%3A%222023%22';
-export const TOTAL_COUNT_API_URL =
-  'https://odre.opendatasoft.com/api/explore/v2.1/catalog/datasets/eco2mix-national-tr/records?limit=1&refine=date_heure%3A%222023%22';
-export const ECO2_MIX_NATIONAL_DATASET = 'eco2mix-national-tr/';
-export const ECO2_MIX_NATIONAL_DATASET_PER_DAY =
-  '/api/explore/v2.1/catalog/datasets/eco2mix-national-tr/records?select=consommation%2Cfioul%2Cgaz%2Cnucleaire%2Ceolien%2Chydraulique%2Cbioenergies%2Ctaux_co2&limit=100&refine=date_heure';
-export const DATA_LIMIT = 100;
-export const SELECT_FIELDS =
-  'records?select=fioul%2C%20charbon%2Cgaz%2Cnucleaire%2Cdate%2Cconsommation';
-
 /* Limite database */
 export const LIMIT_START_DATE_DATA = 'June 06, 2022 00:00:0';
-
-/* Pays frontaliers */
-
-export const ech_comm_angleterre = 'Angleterre';
-export const ech_comm_espagne = 'Espagne';
-export const ech_comm_italie = 'Italie';
-export const ech_comm_suisse = 'Suisse';
-export const ech_comm_allemane_belgique = 'Allemagne et Belgique';
