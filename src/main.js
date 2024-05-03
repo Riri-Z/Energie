54aa58321a77bc67236bd58977064c4b983ef0b5
Riri-Z <riri.zamor@gmail.com>
2024-05-03 11:06:18 +0200

    Add export module

--- src/main.js
+++ src/main.js
@@ -4,10 +4,12 @@ import { createPinia } from 'pinia';
 import router from './router';
 import Highcharts from 'highcharts';
 import Maps from 'highcharts/modules/map';
+import exporting from 'highcharts/modules/exporting';
 import HighchartsVue from 'highcharts-vue';
 import App from './App.vue';
 
 Maps(Highcharts);
+exporting(Highcharts);
 const app = createApp(App);
 app.use(router);
 app.use(HighchartsVue);
