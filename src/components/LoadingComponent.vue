b69033886608819f1d9e1bfbd444372798aca9dd
Riri-Z <riri.zamor@gmail.com>
2024-05-06 15:31:21 +0200

    Add LoadingComponent and fix css

---
+++ src/components/LoadingComponent.vue
@@ -0,0 +1,27 @@
+<script setup></script>
+
+<template>
+  <div class="loader"></div>
+</template>
+
+<style lang="scss" scoped>
+.loader {
+  margin: auto;
+  border: 20px solid $white;
+  border-radius: 50%;
+  border-top: 20px solid $turquoise;
+  width: 100px;
+  height: 100px;
+  animation: spinner-9770a80b 4s linear infinite;
+  top: 20vh;
+  position: relative;
+}
+@keyframes spinner {
+  0% {
+    transform: rotate(0deg);
+  }
+  100% {
+    transform: rotate(360deg);
+  }
+}
+</style>
